3f654f85478799ef8ba28dd2d5563649d0ff4f79
HankDeTank05 <henryhankholman@gmail.com>
2023-02-27 17:04:08 -0600

    created base engine class

---
+++ ZombieShooterRoguelike/TravellerEngine.cpp
@@ -0,0 +1,23 @@
+#include "TravellerEngine.h"
+
+TravellerEngine::TravellerEngine()
+{
+	sAppName = "Zombie Shooter Roguelike";
+}
+
+TravellerEngine::~TravellerEngine()
+{
+	// CODE GOES HERE
+}
+
+bool TravellerEngine::OnUserCreate()
+{
+	// TODO: fill out TravellerEngine::OnUserCreate() function
+	return true;
+}
+
+bool TravellerEngine::OnUserUpdate(float fElapsedTime)
+{
+	// TODO: fill out TravellerEngine::OnUserUpdate() function
+	return true;
+}
